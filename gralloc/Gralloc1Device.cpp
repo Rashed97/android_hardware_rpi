15422139f7577b39faf3c7d6b96e54323afd2e1a
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-01 21:19:48 -0400

    gralloc: Enable proper GetCapabilities function

--- gralloc/Gralloc1Device.cpp
+++ gralloc/Gralloc1Device.cpp
@@ -36,7 +36,7 @@ int Gralloc1Device::CloseDevice(hw_device_t *device) {
 	return 0;
 }
 
-void Gralloc1Device::GetCapabilities(struct gralloc1_device */*device*/, uint32_t *outCount,
+void Gralloc1Device::GetCapabilities(struct gralloc1_device *device, uint32_t *outCount,
                                   int32_t /*gralloc1_capability_t*/ *outCapabilities) {
 	if (outCapabilities == nullptr) {
 		*outCount = 1;
